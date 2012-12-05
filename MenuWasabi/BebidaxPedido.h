1acfc9785cb4c6679f9bc72e860867bc7880480c
Vladimir Guzman <guzman.vla@gmail.com>
2012-12-05 17:08:26 -0500

    FFuncionalidad agregar bebidas lista - Falta parte visual en el menú y corregir que aparecen muchos botones agregar

---
+++ MenuWasabi/BebidaxPedido.h
@@ -0,0 +1,13 @@
+//
+//  BebidaxPedido.h
+//  MenuWasabi
+//
+//  Created by Giovanni Lopez on 12/5/12.
+//  Copyright (c) 2012 3dementes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BebidaxPedido : NSObject
+
+@end
---
+++ MenuWasabi/BebidaxPedido.m
@@ -0,0 +1,13 @@
+//
+//  BebidaxPedido.m
+//  MenuWasabi
+//
+//  Created by Giovanni Lopez on 12/5/12.
+//  Copyright (c) 2012 3dementes. All rights reserved.
+//
+
+#import "BebidaxPedido.h"
+
+@implementation BebidaxPedido
+
+@end
