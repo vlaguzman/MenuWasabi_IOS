7643481d6072ba977e52725d23a9f71c14d4995d
Vladimir Guzman <guzman.vla@gmail.com>
2012-12-06 16:44:39 -0500

    Archivos servidor para bebidas hechos probados

--- server files/consultar_detallepedido.php
+++ server files/consultar_detallepedido.php
@@ -7,7 +7,7 @@ $estado = "solicitado";
 $conexion = mysql_connect("localhost", "brain140_adminwb", "Lounge140") or die ("Error conexión BD");
 	mysql_select_db('brain140_wasabi', $conexion)or die('No se encuentra la base de datos');
 
-	$consulta_detalles = sprintf("SELECT t1.id_plato, t2.nombre, t2.precio, t1.cantidad FROM wsb_platoxpedido AS t1, wsb_plato AS t2
+	$consulta_detalles = sprintf("SELECT t1.id_plato, t2.nombre, t1.total_parcial, t1.cantidad FROM wsb_platoxpedido AS t1, wsb_plato AS t2
        WHERE t1.id_plato = t2.id_plato AND t1.id_pedido='%s'", $pedido);
 	$resultado_detalles = mysql_query($consulta_detalles, $conexion) or die ('Error en SQL: '.$consulta_detalles);
 
@@ -22,6 +22,7 @@ $conexion = mysql_connect("localhost", "brain140_adminwb", "Lounge140") or die (
 		}
 	
 	}
+
 ?>
 
 
